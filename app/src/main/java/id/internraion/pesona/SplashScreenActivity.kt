7643c567d9eb476d5f638bf9217312197b14b112
Farlz <farrelarvino1@gmail.com>
2021-03-24 21:53:00 +0700

    Initial commit, splash screen & font forturn

---
+++ app/src/main/java/id/internraion/pesona/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package id.internraion.pesona
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val timeout = 1500L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(mainLooper).postDelayed(
+            {
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            },timeout
+        )
+    }
+}
