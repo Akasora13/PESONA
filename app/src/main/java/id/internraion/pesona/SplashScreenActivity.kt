5c38ba81ff742db9e5709dcd64310ed6e0185207
Farlz <farrelarvino1@gmail.com>
2021-03-26 22:14:33 +0700

    benerin fullscreen splashscreen

--- app/src/main/java/id/internraion/pesona/SplashScreenActivity.kt
+++ app/src/main/java/id/internraion/pesona/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -11,6 +12,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_splash_screen)
 
         Handler(mainLooper).postDelayed(
