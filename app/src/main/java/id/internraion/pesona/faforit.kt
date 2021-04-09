db6c000103ebf57b320b1f8017837fe3d530b928
alfinzaenalkamala <alfinzaenalkamala@gmail.com>
2021-04-10 06:16:51 +0700

    tambah intent, icon, dependenci firebase, coment ui

--- app/src/main/java/id/internraion/pesona/faforit.kt
+++ app/src/main/java/id/internraion/pesona/faforit.kt
@@ -1,11 +1,19 @@
 package id.internraion.pesona
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_faforit.*
+import kotlinx.android.synthetic.main.activity_home.*
 
 class faforit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_faforit)
+        detailfaf.setOnClickListener {
+            // Handler code here.
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent);
+        }
     }
 }
